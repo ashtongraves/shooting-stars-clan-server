35bc7ca4bbe9ca99d9454c40f0833c34fc4261e5
Andrew McAdams <andmcadams@gmail.com>
2021-11-20 15:11:38 -0600

    Use env vars

--- server.py
+++ server.py
@@ -21,21 +21,11 @@ def create_app(conn: sqlite3.Connection, clazz):
     return app
 
 
-def create_password_app(conn: sqlite3.Connection):
-    app = falcon.App()
-    # Resources are represented by long-lived class instances
-    shooting_stars_resource = PasswordStarMinersResource(conn)
-    app.add_route('/shooting_stars', shooting_stars_resource)
-    app.add_route('/audit', shooting_stars_resource, suffix='separate')
-    app.add_route('/whitelist', shooting_stars_resource, suffix='whitelist')
-    return app
-
-
 if __name__ == '__main__':
     server_conn = sqlite3.connect(os.environ['SHOOTING_STARS_DB'])
     server_conn.row_factory = sqlite3.Row
-    with make_server('', 8000, create_password_app(server_conn)) as httpd:
-        print('Serving on port 8000...')
+    with make_server('', int(os.environ['SHOOTING_STARS_PORT']), create_app(server_conn, BaseShootingStarsResource)) as httpd:
+        print(f'Serving on port {os.environ["SHOOTING_STARS_PORT"]}...')
 
         # Serve until process is killed
         httpd.serve_forever()
